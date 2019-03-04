117b7e10d7e9e1e61c60bd24b19e2f37ad3763a9
Saurabh Premraj <spremraj@nextjump.com>
2019-03-04 16:58:14 -0500

    fixing a typo in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -o testproject -i -v - ldflags="-X main.version=$($(git describe --always --long)" .
+go build -o testproject -i -v - ldflags="-X main.version=$(git describe --always --long)" .
